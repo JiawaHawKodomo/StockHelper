41dd27bb1dd6ee0d591b42c14edd134130f8559c
kodomo <260386109@qq.com>
2020-02-12 12:18:52 +0800

    startup

---
+++ src/main/java/com/kodomo/stockhelper/StockhelperApplication.java
@@ -0,0 +1,13 @@
+package com.kodomo.stockhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockhelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockhelperApplication.class, args);
+    }
+
+}
